6fe7cecc36d5bd3786a284f47d8bdf1b4861133f
mihir-x <ictlancerx@gmail.com>
2023-12-11 18:10:46 +0600

    fixed resume page style

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -44,12 +44,12 @@ const About = () => {
                     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">MD. MIHIR HASAN</h5>
                     <span className="text-sm text-gray-500 dark:text-gray-400">Web Developer</span>
                     <div className=" max-w-lg text-center my-5 md:my-10">
-                        <p>Hi, I am Mihir Hasan. I&apos;ve come from a small village named Sheikhpara. I am a Frontend Web Developer. I like to code and love to read. </p>
+                        <p className=" text-gray-600">Hi, I am Mihir Hasan. I&apos;ve come from a small village named Sheikhpara. I am a Frontend Web Developer. I like to code and love to read. </p>
                     </div>
                     <div className="mt-4 flex flex-col md:flex-row md:gap-20 lg:mt-6">
-                        <div className="flex-1">
+                        <div className="flex-1 border-r-2">
                             <h1 className=" text-lg md:text-2xl font-bold">My Academic qualification:</h1>
-                            <p className="mt-3">M.Sc. in Information and Communications Technology (2016-2017),
+                            <p className="mt-3 text-gray-600">M.Sc. in Information and Communications Technology (2016-2017),
                                 Islamic University, Kushtia. <br /> <br />
                                 B.Sc. in Information and Communications Technology (2013-2016),
                                 Islamic University, Kushtia.
@@ -57,7 +57,7 @@ const About = () => {
                         </div>
                         <div className="flex-1">
                             <h1 className=" text-lg md:text-2xl font-bold">My Experience:</h1>
-                            <p className="mt-3">I have worked as a freelancer in my friend&apos;s company Rubel Design Solution from 2021-2022. <br /> <br /> I am learning web development past 6 months. In this period I worked on many projects including medical camp management system, blog website, brand shop etc. I am specialized in react.js. Mainly I work on MERN stack projects. <br /> <br /> I also have experience in the field of IOT. In my academic year I worked on few IOT projects like home automation, obstacle finder using sonar sensor etc.</p>
+                            <p className="mt-3 text-gray-600">I have worked as a freelancer in my friend&apos;s company Rubel Design Solution from 2021-2022. <br /> <br /> I am learning web development past 6 months. In this period I worked on many projects including medical camp management system, blog website, brand shop etc. I am specialized in react.js. Mainly I work on MERN stack projects. <br /> <br /> I also have experience in the field of IOT. In my academic year I worked on few IOT projects like home automation, obstacle finder using sonar sensor etc.</p>
                         </div>
                     </div>
                 </div>
--- src/Pages/Resume/Resume.jsx
+++ src/Pages/Resume/Resume.jsx
@@ -24,15 +24,15 @@ const Resume = () => {
                 <h1 className=" text-xl md:text-3xl font-bold">MD. MIHIR HASAN</h1>
                 <p>Frontend Web Developer</p>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-40 mt-5 md:mt-20">
-                <div className="flex-1">
+            <div className="flex flex-col md:flex-row md:gap-40 mt-5 md:mt-20 border-b-2">
+                <div className="flex-1 md:border-r-2 md:p-5">
                     <h1 className=" text-lg md:text-2xl font-bold">About Me:</h1>
                     <p>I am MD. Mihir Hasan, Front-End Web Developer with expertise in the MERN stack, hands-on
                         experience in creating responsive and user-friendly web applications. Skilled in front-end
                         technologies such as JavaScript, React.js, HTML5, CSS3 combined with back-end development
                         using Node.js and Express.js.</p>
                 </div>
-                <div className=" mt-5 md:mt-0 flex-1">
+                <div className=" mt-5 md:mt-0 flex-1 md:p-5">
                     <h1 className=" text-lg md:text-2xl font-bold">My Contact:</h1>
                     <p className="mt-3"><span className="font-bold">Address:</span> Jhenaidah, Dhaka, Bangladesh <br />
                         <span className="font-bold">Email:</span> mihirhx@gmail.com <br />
@@ -42,8 +42,8 @@ const Resume = () => {
                     </p>
                 </div>
             </div>
-            <div className="flex flex-col md:flex-row md:gap-40 mt-5 md:mt-20">
-                <div className="flex-1">
+            <div className="flex flex-col md:flex-row md:gap-40 mt-5 md:mt-20 border-b-2">
+                <div className="flex-1 md:border-r-2 md:p-5">
                     <h1 className=" text-lg md:text-2xl font-bold">Technical Skills:</h1>
                     <p className="mt-3"><span className="font-bold">Expert:</span> React.js, JavaScript, HTML5, CSS3, Tailwind CSS, Firebase, MongoDB <br />
                         <span className="font-bold">Comfortable:</span> Node.js, Express.js <br />
@@ -51,7 +51,7 @@ const Resume = () => {
                         <span className="font-bold">Familiar:</span> Stripe.js, Next.js, Material UI <br />
                     </p>
                 </div>
-                <div className=" mt-5 md:mt-0 flex-1">
+                <div className=" mt-5 md:mt-0 flex-1 md:p-5">
                     <h1 className=" text-lg md:text-2xl font-bold">Interpersonal Skills:</h1>
                     <p className="mt-3">Adaptability, Perseverance, Quick learner, Teamwork, Responsibility
                     </p>
@@ -59,35 +59,35 @@ const Resume = () => {
             </div>
             <div className="mt-5 md:mt-20">
                 <h1 className=" text-center mb-5 text-lg md:text-2xl font-bold">Projects:</h1>
-                <div className=" grid grid-cols-1 md:grid-cols-3 gap-5">
-                    <div>
-                        <h1 className=" text-lg md:text-xl font-bold">Medical Camp Management (MERN Stack)</h1>
-                        <a href="https://github.com/mihir-x/medical-camp-management-project" className="text-blue-800">GitHub Link</a>
-                        <a href="https://luminous-sherbet-6eb0c1.netlify.app" className="text-blue-800 ml-5">Live Link</a>
+                <div className=" grid grid-cols-1 md:grid-cols-3 gap-5 border-b-2 md:p-5">
+                    <div className=" md:border-r-2">
+                        <h1 className=" text-lg md:text-xl font-bold mb-2">Medical Camp Management</h1>
+                        <a href="https://github.com/mihir-x/medical-camp-management-project" className="text-white p-1 rounded-md bg-gray-500">GitHub Link</a>
+                        <a href="https://luminous-sherbet-6eb0c1.netlify.app" className="text-white p-1 rounded-md bg-gray-500 ml-5">Live Link</a>
                         <p className="font-bold">Features:</p>
-                        <ul>
+                        <ul className="list-disc p-2">
                             <li>Database integration with mongoDB</li>
                             <li>Payment system integration with stripe.js</li>
                             <li>Login/Registration with Firebase</li>
                         </ul>
                     </div>
-                    <div>
-                        <h1 className=" text-lg md:text-xl font-bold">Blog Hub (MERN Stack)</h1>
-                        <a href="https://github.com/mihir-x/my-blog-website-assignment" className="text-blue-800">GitHub Link</a>
-                        <a href="https://gorgeous-kitten-b026ab.netlify.app" className="text-blue-800 ml-5">Live Link</a>
+                    <div className=" md:border-r-2">
+                        <h1 className=" text-lg md:text-xl font-bold mb-2">Blog Hub</h1>
+                        <a href="https://github.com/mihir-x/my-blog-website-assignment" className="text-white p-1 rounded-md bg-gray-500">GitHub Link</a>
+                        <a href="https://gorgeous-kitten-b026ab.netlify.app" className="text-white p-1 rounded-md bg-gray-500 ml-5">Live Link</a>
                         <p className="font-bold">Features:</p>
-                        <ul>
+                        <ul className="list-disc p-2">
                             <li>Database integration with mongoDB</li>
                             <li>Can add, update blog and post comment</li>
                             <li>Login/Registration with Firebase</li>
                         </ul>
                     </div>
                     <div>
-                        <h1 className=" text-lg md:text-xl font-bold">Medical Camp Management (MERN Stack)</h1>
-                        <a href="https://github.com/mihir-x/automotive-brand-shop-project" className="text-blue-800">GitHub Link</a>
-                        <a href="https://celadon-croissant-bebbb3.netlify.app" className="text-blue-800 ml-5">Live Link</a>
+                        <h1 className=" text-lg md:text-xl font-bold mb-2">Brand Shop</h1>
+                        <a href="https://github.com/mihir-x/automotive-brand-shop-project" className="text-white p-1 rounded-md bg-gray-500">GitHub Link</a>
+                        <a href="https://celadon-croissant-bebbb3.netlify.app" className="text-white p-1 rounded-md bg-gray-500 ml-5">Live Link</a>
                         <p className="font-bold">Features:</p>
-                        <ul>
+                        <ul className="list-disc p-2">
                             <li>Database integration with mongoDB</li>
                             <li>Dark/light theme integration on homepage</li>
                             <li>Login/Registration with Firebase</li>
